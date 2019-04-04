1dd92e00e7ef6975ae1925b52f64d2bcb65d2c22
davescott193 <davescott193@users.noreply.github.com>
2019-04-04 14:59:06 +1300

    My game so far

---
+++ Assets/Scripts/Movement.cs
@@ -0,0 +1,25 @@
+﻿
+using UnityEngine;
+
+public class Movement : MonoBehaviour { 
+    public Rigidbody rb;
+
+    public float forwardforce = 2000f;
+    public float sidewaysForce = 500f;
+
+    //deltatime allows for framerate leniancy. 
+    void FixedUpdate()
+    {
+        rb.AddForce(0, 0, forwardforce * Time.deltaTime);
+
+        if (Input.GetKey("d") )
+        {
+            rb.AddForce(sidewaysForce * Time.deltaTime, 0, 0);
+        }
+        if (Input.GetKey("a") )
+        {
+            rb.AddForce(-sidewaysForce * Time.deltaTime, 0, 0);
+        }
+
+    }
+}
---
+++ Assets/Scripts/counter.cs
@@ -0,0 +1,11 @@
+﻿using UnityEngine;
+using UnityEngine.UI;
+public class counter : MonoBehaviour{
+
+    public Transform player;
+    public Text scoreText;
+    void Update()
+    {
+        scoreText.text = player.position.z.ToString("0");
+    }
+}
